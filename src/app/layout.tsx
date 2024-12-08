69feb7a512a7a5ea765c555ef1409eb7d84897ae
Nithin Sudersan <nithinis@gmail.com>
2024-12-08 01:26:19 -0500

    fix content height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-full`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-lvh`}
       >
         {children}
       </body>
